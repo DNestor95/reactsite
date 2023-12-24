f43c0dcb5b906a77e8580ab0802f5bdf978bbe5d
Daniel Nestor <daniel.nestor.jr@gmail.com>
2023-12-24 12:09:58 -0500

    main site working
    have the base single webpage working and basic components made

---
+++ reactsite/src/components/MainContent.js
@@ -0,0 +1,13 @@
+// src/components/MainContent.js
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main>
+      <p>Welcome to my page </p>
+      <p>This page will display my portfolio and give everyone a place to reach me if you want to work with me on any projects.</p>
+    </main>
+  );
+};
+
+export default MainContent;
